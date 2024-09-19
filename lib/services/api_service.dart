e039709e453ff3996a63a3bf9d813c5be2681ba4
salmanbashar1009 <salmanbashar101@gmail.com>
2024-09-19 13:28:54 +0600

    api_service added

---
+++ lib/services/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:weather_app_1/consts/urls.dart';
+import 'package:weather_app_1/models/weather_model.dart';
+
+class ApiService {
+  Future<WeatherModel> getWeatherData(String searchText) async {
+    String url = "$BaseUrl&q=$searchText&days=7";
+
+    try {
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        WeatherModel weatherModel = WeatherModel.fromJson(json);
+        return weatherModel;
+      } else {
+        throw "No Data found";
+      }
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
