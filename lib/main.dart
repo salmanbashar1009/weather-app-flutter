e657d073015101a3b96a89ffc910ef7400afb4df
salmanbashar1009 <salmanbashar101@gmail.com>
2024-09-19 12:07:29 +0600

    weather_app_1 project created

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_1/ui/home_screen.dart';
+
+void main() {
+  runApp(const MyWeatherApp());
+}
+
+class MyWeatherApp extends StatelessWidget {
+  const MyWeatherApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "My Weather App",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+
