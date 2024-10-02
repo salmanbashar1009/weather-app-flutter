c490e48a90471f32818509309cca331a5547701b
salmanbashar1009 <salmanbashar101@gmail.com>
2024-10-02 12:07:34 +0600

    hourly weather section completed

--- lib/ui/components/hourly_weather_list_item.dart
+++ lib/ui/components/hourly_weather_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_app_1/models/weather_model.dart';
 
 class HourlyWeatherListItem extends StatelessWidget {
@@ -14,8 +15,40 @@ class HourlyWeatherListItem extends StatelessWidget {
       margin: const EdgeInsets.all(8),
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
-          color: Colors.white24, borderRadius: BorderRadius.circular(20),),
+        color: Colors.white24,
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  hour?.tempC?.round().toString() ?? ' ',
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Text("o",style: TextStyle(color: Colors.white),),
+            ],
+          ),
+          Container(
+            decoration: const BoxDecoration(shape: BoxShape.circle,color: Colors.teal),
+            height: 50,
+            child: Image.network("https:${hour?.condition?.icon.toString()}"),
+          ),
+          Text(DateFormat.j().format(DateTime.parse(hour?.time?.toString() ?? " ",),),style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              ),)
+        ],
       ),
     );
   }
